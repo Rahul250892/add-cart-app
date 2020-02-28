21d4fca36c6640a0763af6e34577ae6c0c3cd309
rahul250892 <rahulkumar250892@outlook.com>
2020-02-28 19:11:36 +0530

    Updates

---
+++ precache-manifest.c131ce611f23f46cf1404aab70e62578.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1363f96c5b4526e676b1",
+    "url": "/add-cart-app/static/js/runtime~main.1363f96c.js"
+  },
+  {
+    "revision": "d828bfcfb41308003488",
+    "url": "/add-cart-app/static/js/main.d828bfcf.chunk.js"
+  },
+  {
+    "revision": "6462784eedb5d9281a5c",
+    "url": "/add-cart-app/static/js/1.6462784e.chunk.js"
+  },
+  {
+    "revision": "d828bfcfb41308003488",
+    "url": "/add-cart-app/static/css/main.5fa26772.chunk.css"
+  },
+  {
+    "revision": "ef8a84e1d13cd305f8b565110208c754",
+    "url": "/add-cart-app/index.html"
+  }
+];
